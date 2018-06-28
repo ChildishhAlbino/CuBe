a67af994bfa90511712d7cb7f095c79c58bd376d
ChildishhAlbino <connorwilliams.home@gmail.com>
2018-06-28 22:42:55 +1000

    Input: Added InputManager Interface and surrounding classes

---
+++ CuBe/Assets/Controller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public Cube cube;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void RotateCube()
+    {
+        cube.transform.Rotate(new Vector3(45, 90, 0));
+    }
+}
+
---
+++ CuBe/Assets/IInputManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInputManager
+{
+    void CheckForInput();
+    bool IsTouch();
+    void ParseSwipe(Swipe swipe);
+}
+
+public enum Swipe
+{
+    up,
+    down,
+    left,
+    right
+}
