23f380b04e0da31df9e360f1caf0ffe623979506
ChildishhAlbino <connorwilliams.home@gmail.com>
2018-06-28 22:52:33 +1000

    INPUT: Added InputManagerFactory and Touch / M&Kboard inputs

--- CuBe/Assets/Controller.cs
+++ CuBe/Assets/Controller.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    private IInputManager inputManager;
     public Cube cube;
     // Use this for initialization
     void Start()
@@ -13,12 +14,17 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        inputManager.CheckForInput();
     }
 
     public void RotateCube()
     {
         cube.transform.Rotate(new Vector3(45, 90, 0));
+    }
+
+    private void GetInputManager()
+    {
+        inputManager = InputManagerFactory.BuildInputManager();
     }
 }
 
--- CuBe/Assets/IInputManager.cs
+++ CuBe/Assets/IInputManager.cs
@@ -9,6 +9,57 @@ public interface IInputManager
     void ParseSwipe(Swipe swipe);
 }
 
+public static class InputManagerFactory
+{
+    public static IInputManager BuildInputManager()
+    {
+        if (Application.isMobilePlatform)
+        {
+            return new TouchInputManager();
+        }
+        else
+        {
+            return new MouseAndKeyboardInputManager();
+        }
+    }
+}
+
+public class MouseAndKeyboardInputManager : IInputManager
+{
+    public void CheckForInput()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool IsTouch()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ParseSwipe(Swipe swipe)
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+public class TouchInputManager : IInputManager
+{
+    public void CheckForInput()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool IsTouch()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ParseSwipe(Swipe swipe)
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
 public enum Swipe
 {
     up,
